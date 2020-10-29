6f38d49da961d3c87e9034094868e5b4eeee3bec
lahirutech <rmllbrathnayaka@gmail.com>
2020-10-29 21:41:06 +0530

    Adding the admin route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const admin = require("./routes/api/admin");
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+app.use("/api/admin",admin);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
