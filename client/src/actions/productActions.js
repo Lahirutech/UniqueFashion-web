8de0ecbb2dce36c2510a599149d8b3d9420fc558
lahirutech <rmllbrathnayaka@gmail.com>
2020-10-31 22:56:55 +0530

    Creating update product action

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -2,14 +2,26 @@
 import axios from "axios";
 import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 
-export const addProductdata = (productData, history) => dispatch => {
-    axios
-      .post("/api/admin/addProduct", productData)
-      .then(res => history.push("/login"))
-      .catch(err =>
-        dispatch({
-          type: GET_ERRORS,
-          payload: err.response.data
-        })
-      );
-  };
+export const addProductdata = (productData, history) => (dispatch) => {
+  axios
+    .post("/api/admin/addProduct", productData)
+    .then((res) => history.push("/login"))
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+export const updateProductdata = (productData, history) => (dispatch) => {
+  axios
+    .put("/api/admin/updateProduct", productData)
+    .then((res) => history.push("/viewmyproducts"))
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
